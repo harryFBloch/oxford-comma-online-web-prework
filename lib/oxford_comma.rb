dd42d1fd155f4010745f0bc090346f16a693866d
Harry Bloch <harryfbloch@gmail.com>
2018-09-19 23:02:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  newString = ""
+  if array.length > 2
+    array.each_with_index {|element, index| 
+      if index != array.length
+        newString.concat(", #{element}")
+      else
+        newString.concat(", and #{element}")
+      end
+    }
+  elsif array.length == 2
+    newString = "#{array[0]} and #{array[1]}"
+  else
+    newString = array[0]
+  end
 end
